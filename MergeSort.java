4f8a65f8fcb4558e9efcc18028caf59afddb304e
Dhruvam Dhruvil <69448775+Dhruvam17@users.noreply.github.com>
2023-10-16 09:59:38 +0530

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,70 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        mergeSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+
+    public static void mergeSort(int[] arr) {
+        int n = arr.length;
+        if (n < 2) {
+            return;
+        }
+
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            right[i - mid] = arr[i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+        int n1 = left.length;
+        int n2 = right.length;
+        int i = 0, j = 0, k = 0;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
